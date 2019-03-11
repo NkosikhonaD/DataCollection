8c6350e5d32795836126c159e78ad605644d46b5
Nkosikhona <dlamininkd@gmail.com>
2019-03-11 16:13:50 +0200

    save lion iimage method impleted: Only to get name of body part

--- lionscrap.py
+++ lionscrap.py
@@ -20,6 +20,41 @@ def is_good_response(resp):
     content_type = resp.headers['Content-Type'].lower()
     return (resp.status_code ==200 and content_type is not None and content_type.find('html')>-1)
 
+def save_lion_image(lion_list):
+
+    for name in lion_list:
+        print("-------------------------------", name, "---------------------------")
+        url = "http://livingwithlions.org/mara/lions/"+name+"/"
+        raw_html = simple_get(url)
+        if raw_html is not None:
+            html = BeautifulSoup(raw_html, 'html.parser')
+            image_div = html.find_all("div", {"id":"tabs-1"})
+            for item in image_div:
+
+                body_part_list = item.ul
+                for part in body_part_list:
+                    try:
+                        body_part = part.a.text
+                        print(body_part)
+                    except:
+                        continue
+        else:
+            print("URL IS None")
+
+
+def get_lions_list(url):
+    lion_list = []
+    raw_html = simple_get(url)
+    if raw_html is not None:
+        html = BeautifulSoup(raw_html, 'html.parser')
+        lion_div = html.find_all("div", {"class": "resultBox"})
+        for each_lion in lion_div:
+            lion_name = each_lion.h2.a.text
+            lion_name = lion_name.rstrip()
+            lion_name = lion_name.lstrip()
+            lion_list.append(lion_name)
+    return lion_list
+
 def get_data_features(url):
     raw_html =simple_get(url)
 
@@ -32,7 +67,6 @@ def get_data_features(url):
             data = ""
             data=data+lion_name
             feature_list = each_lion.ul
-            #print(lion_name)
             for li in feature_list:
                 if li is not None:
                     try:
@@ -48,6 +82,8 @@ def log_error(e):
 if __name__ =='__main__':
     print('Getting Lion features from living with lions site\n')
     url = 'http://livingwithlions.org/mara/browse/all/all/'
-    get_data_features(url)
+    lion_names = get_lions_list(url)
+    save_lion_image(lion_names)
+    #get_data_features(url)
     print('Done \n')
 
