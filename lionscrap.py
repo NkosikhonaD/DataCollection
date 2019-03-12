1a9b0c061d4d07bf44b0de39246969ac0f918997
Nkosikhona <dlamininkd@gmail.com>
2019-03-12 13:51:00 +0200

    finding image url

--- lionscrap.py
+++ lionscrap.py
@@ -29,20 +29,26 @@ def save_lion_image(lion_list):
         if raw_html is not None:
             html = BeautifulSoup(raw_html, 'html.parser')
             image_div = html.find_all("div", {"id":"tabs-1"})
+
             for item in image_div:
 
                 body_part_list = item.ul
+
                 for part in body_part_list:
                     try:
                         body_part = part.a.text
-                        print(body_part)
+                        body_part = body_part.rstrip()
+                        body_part = body_part.lstrip()
+                        body_part_image = part.a.img['src']
+                        print(body_part, body_part_image)
                     except:
                         continue
         else:
             print("URL IS None")
 
-
 def get_lions_list(url):
+    print('Getting Lion names \n')
+
     lion_list = []
     raw_html = simple_get(url)
     if raw_html is not None:
@@ -57,30 +63,29 @@ def get_lions_list(url):
 
 def get_data_features(url):
     raw_html =simple_get(url)
-
+    print('Getting Lion features from living with lions site \n')
     if raw_html is not None:
         html = BeautifulSoup(raw_html,'html.parser')
         lion_div = html.find_all("div",{"class":"resultBox"})
 
         for each_lion in lion_div:
             lion_name = each_lion.h2.a.text
+            lion_name= lion_name.rstrip()
+            lion_name = lion_name.lstrip()
             data = ""
-            data=data+lion_name
+            data = data+lion_name
             feature_list = each_lion.ul
             for li in feature_list:
                 if li is not None:
                     try:
                        data=data+","+(li.text[li.text.rfind(':')+1:].rstrip().lstrip())
-
                     except:
                         continue
             print(data)
-
 def log_error(e):
     print(e)
 
 if __name__ =='__main__':
-    print('Getting Lion features from living with lions site\n')
     url = 'http://livingwithlions.org/mara/browse/all/all/'
     lion_names = get_lions_list(url)
     save_lion_image(lion_names)
